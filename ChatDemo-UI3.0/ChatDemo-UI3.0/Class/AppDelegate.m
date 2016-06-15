d5dbb00d1756aabc0a1a88dea88de7d4ff58c369
Jerry Wu <jerrywu12@gmail.com>
2016-06-15 10:16:15 -0700

    fix color

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/AppDelegate.m
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/AppDelegate.m
@@ -34,7 +34,7 @@ static NSString *const kTrackingPreferenceKey = @"allowTracking";
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
 
-    [[UINavigationBar appearance] setBarTintColor:[UIColor HIColorGreenMajor]];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor HIPrimaryColor]];
     [[UINavigationBar appearance] setTitleTextAttributes:
      [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, [UIFont fontWithName:@"HelveticaNeue" size:21.0], NSFontAttributeName, nil]];
     
